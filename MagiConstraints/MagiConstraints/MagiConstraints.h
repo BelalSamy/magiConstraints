c3d18b6bf27b0f2ed42226d4bf8766ecb8e016b0
Belal Samy <belalsamy10@gmail.com>
2021-02-08 15:46:49 +0200

    add framework

---
+++ MagiConstraints/MagiConstraints/MagiConstraints.h
@@ -0,0 +1,19 @@
+//
+//  MagiConstraints.h
+//  MagiConstraints
+//
+//  Created by Belal Samy on 2/8/21.
+//  Copyright © 2021 Belal Samy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MagiConstraints.
+FOUNDATION_EXPORT double MagiConstraintsVersionNumber;
+
+//! Project version string for MagiConstraints.
+FOUNDATION_EXPORT const unsigned char MagiConstraintsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MagiConstraints/PublicHeader.h>
+
+
